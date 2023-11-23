be379cb947c234192594d06c66d045ab50200372
KotaKarthik1 <karthikkotaps1@gmail.com>
2023-11-23 10:59:38 +0530

    firstcommit

---
+++ src/main/java/mavenjava1/App.java
@@ -0,0 +1,14 @@
+package mavenjava1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("karthik kota");
+    }
+}
